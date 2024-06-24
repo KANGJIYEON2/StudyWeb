b6c23860edf7e0c85bdeb771c663928ee2065995
KANGJIYEON2 <hihello516@naver.com>
2024-06-24 17:25:02 +0900

    update

---
+++ dummmy.js
@@ -0,0 +1,25 @@
+// //구조분해할당 배열
+// const color = ["pink", "purple", "green", "yellow", "blue"];
+
+// color.push = "aquagreen";
+
+// const [a, b] = color;
+
+// console.log(a, b);
+
+// //구조분해 할당 객체
+
+// const user = [{
+//   isLoggind: true,
+//   name: "홍길동",
+//   age: "24",
+//   gender: "male",
+//   role: "admin",
+
+// },];
+
+// const { name, age } = user;
+// // const name = user.name
+// // const age = user.age
+// // 와 같음
+// console.log(name, age);
