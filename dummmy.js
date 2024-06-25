c63d168423f9ee3349c44cd0ee08f2ec2bfb09c9
KANGJIYEON2 <hihello516@naver.com>
2024-06-25 10:55:26 +0900

    update

--- dummmy.js
+++ dummmy.js
@@ -23,3 +23,22 @@
 // // const age = user.age
 // // 와 같음
 // console.log(name, age);
+
+const conterNum = document.querySelector(".exchange-num");
+const addNum = document.querySelector(".increase-btn");
+const minusNum = document.querySelector(".decrease-btn");
+let counter = 0;
+addNum.addEventListener("click", () => plusNumber());
+minusNum.addEventListener("click", () => minusNumber());
+
+function plusNumber() {
+  counter = counter + 1;
+  conterNum.innerHTML = `
+ <h1 class="has-text-primary is-size-1 exchange-num">${counter}</h1>`;
+}
+
+function minusNumber() {
+  counter = counter - 1;
+  conterNum.innerHTML = `
+   <h1 class="has-text-primary is-size-1 exchange-num">${counter}</h1>`;
+}
